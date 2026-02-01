e16b6073e1a14a8a2d51ffe84e97d03439764e4f
John Hand <jhandalex100@gmail.com>
2026-01-31 20:40:14 -0500

    Allow Authorization header in CORS

--- api/src/app.js
+++ api/src/app.js
@@ -65,6 +65,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 // Main CORS handler
