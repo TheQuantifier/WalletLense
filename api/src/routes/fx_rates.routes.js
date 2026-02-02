40133a7f074e2878a9abb08c6d72972ab4d5dcc5
John Hand <jhandalex100@gmail.com>
2026-02-02 16:02:40 -0500

    Make fx rates endpoint public

--- api/src/routes/fx_rates.routes.js
+++ api/src/routes/fx_rates.routes.js
@@ -2,11 +2,10 @@
 import express from "express";
 
 import * as controller from "../controllers/fx_rates.controller.js";
-import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 // Shared daily FX rates (cached)
-router.get("/", auth, controller.getLatest);
+router.get("/", controller.getLatest);
 
 export default router;
